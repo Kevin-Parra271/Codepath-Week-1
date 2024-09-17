a9e1a9fcf2dc79518184683a384bfe679e12ece6
Kevin-Parra271 <110420482+Kevin-Parra271@users.noreply.github.com>
2024-09-17 19:39:41 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Calendar from './components/Calendar';
+
+const App = () => {
+  return (
+    <div className="App">
+       day calander in Miami<h1></h1>
+      <h2>Weekly Planner</h2>
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
